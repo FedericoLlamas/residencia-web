418fd0882afb743f9eca167ca76ae34063a34e7f
Federico Llamas <federico@comparaencasa.com>
2020-01-07 20:55:02 -0300

    working

--- js/contact_me.js
+++ js/contact_me.js
@@ -68,3 +68,66 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
+
+
+// function submitToAPI(e) {
+//    e.preventDefault();
+//    // var URL = "http://lzucijp208.execute-api.us-east-1.amazonaws.com/mailfwd";
+
+//         var Namere = /[A-Za-z]{1}[A-Za-z]/;
+//         if (!Namere.test($("#name").val())) {
+//                      alert ("Name can not less than 2 char");
+//             return;
+//         }
+//         var mobilere = /[0-9]{10}/;
+//         if (!mobilere.test($("#phone").val())) {
+//             alert ("Please enter valid mobile number");
+//             return;
+//         }
+//         if ($("#email").val()=="") {
+//             alert ("Please enter your email id");
+//             return;
+//         }
+
+//         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+//         if (!reeamil.test($("#email").val())) {
+//             alert ("Please enter valid email address");
+//             return;
+//         }
+
+//    var name = $("#name").val();
+//    var phone = $("#phone").val();
+//    var email = $("#email").val();
+//    var desc = $("#message").val();
+//    var data = {
+//       name : name,
+//       phone : phone,
+//       email : email,
+//       desc : desc
+//     };
+
+//    $.ajax({
+//      type: "POST",
+//      url : "https://fo3f21dtzh.execute-api.us-east-1.amazonaws.com/mailfwd",
+//      dataType: "json",
+//      headers: {
+//       "Access-Control-Allow-Headers": "Content-Type",
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Methods": "*",
+//     },
+//      crossDomain: "true",
+//      contentType: "application/json; charset=utf-8",
+//      data: JSON.stringify(data),
+
+     
+//      success: function () {
+//        // clear form and show a success message
+//        alert("Successfull");
+//        document.getElementById("contact-form").reset();
+//        location.reload();
+//      },
+//      error: function (error) {
+//        // show an error message
+//        alert("UnSuccessfull");
+//      }});
+//  }
